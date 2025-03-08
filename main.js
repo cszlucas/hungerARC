aeeaff4cdf2407a0cd49f34960b12a752e379c45
zlucas <zlucas@bethel.jw.org>
2025-03-07 23:13:07 -0500

    Need to work on last 2 tmr.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 
 //zoe figure out year factor
-function performRMDs(scenario, rothWithdrawal,RMDStrategyInvestOrder, allInvestments, currYear){
+function performRMDs(scenario, RMDStrategyInvestOrder, currYear){
   userAge = time.Now.Year() - scenario.birthYearUser
-  if userAge>=73 && RMDStrategyInvestOrder!=null: //at least one pretax investment in previous year
-      distributionPeriod = rmdTable(“age”: userAge).distributionPeriod
-      allInvestmentsPreTax = db.investments.query("scenario_id": scenario.id, “accountTaxStatus”: “pre-tax retirement”, "year": currYear-1)
-      allInvestmentsAfterTax = db.investments.query("scenario_id": scenario.id, “accountTaxStatus”: “after-tax retirement”)
+  if(userAge>=73 && RMDStrategyInvestOrder!=null){ //at least one pretax investment in previous year
+      distributionPeriod = rmdTable("age": userAge).distributionPeriod
+      allInvestmentsPreTax = db.investments.query("scenario_id": scenario.id, "accountTaxStatus": "pre-tax retirement": "year": currYear-1)
+      allInvestmentsAfterTax = db.investments.query("scenario_id": scenario.id, "accountTaxStatus": "after-tax retirement")
       sumInvestmentsPreTax = 0
       for (let preTaxInvestment in allInvestmentsPreTax){ //get sum of all pretax investments from previous year
         sumInvestmentsPreTax += preTaxInvestment.value
@@ -15,17 +15,21 @@ function performRMDs(scenario, rothWithdrawal,RMDStrategyInvestOrder, allInvestm
       currYearIncome += rmd
 
       rmdCount = rmd
-      for each preTaxInvest in getRMDStrategyOrder:
-        if rmdCount>0
-        
-          if ( preTaxInvest.value - rmdCount >= 0 ) //able to accomplish rmd with this investment, keep some of old investment and transfer new
+      for( let preTaxInvest in getRMDStrategyOrder){
+        if(rmdCount>0){
+
+          if ( preTaxInvest.value - rmdCount >= 0 ){ //able to accomplish rmd with this investment, keep some of old investment and transfer new
             transferInvestment(preTaxInvest, allInvestmentsAfterTax, rmdCount)
             preTaxInvest.value -= rmdCount
             break
-          else //not able to accomplish rmd with this investment, transfer all of old investment and go to next investment in strategy order
+          } else { //not able to accomplish rmd with this investment, transfer all of old investment and go to next investment in strategy order
             transferInvestment(preTaxInvest, allInvestmentsAfterTax, preTaxInvest.value)
             rmdCount -= preTaxInvest.value
             preTaxInvest.value = 0
+          }
+        }
+    }
+  }
 }
 
 
@@ -52,39 +56,78 @@ function transferInvestment(preTaxInvest, allInvestmentsAfterTax, amountTransfer
 function payNonDiscretionaryExpenses(scenario, previousYearTax,cashInvestment){
   nonDiscretionaryExpenses = db.scenario.query({"_id": scenario.id, expenseEvents.discretionary: false})
   withdrawalStrategy =  db.scenario.query({"_id": scenario.id}).expenseWithdrawalStrategy
-  paymentAmt = getExpensesAndTaxes(nonDiscretionaryExpenses, scenario)
+  paymentAmt = (getExpenses(nonDiscretionaryExpenses) + getTaxes(scenario))
   if (cashInvestment-paymentAmt >= 0){ //can pay from cash investment
     cashInvestment-=paymentAmt
   }
   else{ //get money from investments
     for(let investment in withdrawalStrategy){
         if(paymentAmt>0){
-            paymentAmt -= transferMoney(paymentAmt, investment)
+            paymentAmt -= transferMoneyFromInvestment(paymentAmt, investment, userAge)
         }
     }
   }
 }
 
+function payDiscretionaryExpenses(scenario, cashInvestment){
+    spendingStrategy = db.scenario.query({"_id": scenario.id}).spendingStrategy
+    withdrawalStrategy =  db.scenario.query({"_id": scenario.id}).expenseWithdrawalStrategy
+	for(let expense in spendingStrategy){
+        if (cashInvestment-expense.value >= 0){ //can pay from cash investment
+            cashInvestment-=expense.value
+            expense.value = 0
+            continue
+        }
+		for(let investment in withdrawalStrategy){
+            if(expense.value>0 && (scenario.financialGoal - expense.value >=0 )){
+                expense.value-=transferMoneyFromInvestment(expense.value, investment, userAge)
+            }else if(expense.value>0 && (scenario.financialGoal - expense.value <0 )){ //only pay as much as does not violate financial goal
+                expense.value-=transferMoneyFromInvestment((expense. value-scenario.financialGoal), investment, userAge)
+            }
+        }
+        if(expense.value>0){
+            console.log("You were not able to pay all your discretionary expenses.")
+        }
+    }
+}
 
-function transferMoney(paymentAmt, investment){
-    if (investment.value - paymentAmt >= 0){ //can pay all with this investment
+
+
+function transferMoneyFromInvestment(paymentAmt, investment, userAge){
+    if (investment.value - paymentAmt > 0){ //can pay all with this investment, what about break even?
+        if(investment.accountTaxStatus!="pre-tax retirement"){
+            capitalGain = paymentAmt * (investment.value - getPurchasePrice())
+        }if(investment.accountTaxStatus=="pre-tax retirement"){
+            currYearIncome-=paymentAmt
+        }if((investment.accountTaxStatus=="pre-tax retirement" || investment.accountTaxStatus=="after-tax retirement") && userAge<59){
+            currYearEarlyWithdrawals-=paymentAmt
+        }
+        investment.value-=paymentAmt
         return paymentAmt
     }else{ //use up this investment and move onto next
         if(investment.accountTaxStatus!="pre-tax retirement"){
-            capitalGain = investment.value - getPurchasePrice() //Zoe create this
+            capitalGain = investment.value - getPurchasePrice() //Zoe create this. Purchase price = sum of the amounts of purchases of the investment plus initial value at start of simulation.
+        }if(investment.accountTaxStatus=="pre-tax retirement"){
+            currYearIncome-=investment.value
+        }if((investment.accountTaxStatus=="pre-tax retirement" || investment.accountTaxStatus=="after-tax retirement") && userAge<59){
+            currYearEarlyWithdrawals-=investment.value
         }
+        investment.value = 0
         return investment.value
     }
 }
 
-
-
-function getExpensesAndTaxes(nonDiscretionaryExpenses, scenario){
+function getExpenses(expenses){
   sum = 0
-  for(let expense in nonDiscretionaryExpenses){ //expenses
+  for(let expense in expenses){ //expenses
     sum += expense.value
   }
-  //Taxes below
+  return sum
+}
+
+
+function getTaxes(scenario){
+  sum = 0
   if (scenario.single == true){
     tax = db.tax.single.query({"income": scenario.currYearIncome})
   } else {
